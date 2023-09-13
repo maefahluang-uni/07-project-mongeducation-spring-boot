91a3cd50395fc2732e55691f228ec9d535dd8f07
ohm2303 <95691732+ohm2303@users.noreply.github.com>
2023-09-13 17:01:19 +0700

    created

---
+++ backend/mong-bank-microservice/src/test/java/spring/edu/mongbank/MongBankApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.edu.mongbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
