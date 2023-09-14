0f7c1ab3be40b0cb0ad56c339b57a3e7063e6a4a
Mizpuno <nititorn.certihunt@gmail.com>
2023-09-14 18:26:22 +0700

    updated .vscode config

---
+++ backend/mong-report/src/main/java/spring/edu/mongreport/ServerMapper.java
@@ -0,0 +1,12 @@
+package spring.edu.mongreport;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper (componentModel = "spring")
+public interface ServerMapper {
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    public void updateReportFromDto(ReportDTO dto, @MappingTarget Report entity);
+}
