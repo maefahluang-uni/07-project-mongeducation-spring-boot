b64d9517064e8ff40841c1269d6aa4ebb4e73bed
ohm2303 <95691732+ohm2303@users.noreply.github.com>
2023-09-14 17:37:11 +0700

    del bank

--- backend/mong-bank/src/main/java/spring/edu/mongbank/BankRepository.java
+++
@@ -1,8 +0,0 @@
-package spring.edu.mongbank;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BankRepository extends CrudRepository<Bank,Long>{
-    public List<Bank> findAll();
-}
