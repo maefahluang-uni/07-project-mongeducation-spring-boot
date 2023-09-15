a6c8c93dfdc991565f5f43d8b644a46f87d05a30
Mizpuno <nititorn.certihunt@gmail.com>
2023-09-15 13:27:58 +0700

    added.kafka/ config

--- backend/mong-student/src/main/java/spring/edu/mongstudent/kafka/StudentConfig.java
+++ backend/mong-student/src/main/java/spring/edu/mongstudent/kafka/StudentConfig.java
@@ -18,7 +18,6 @@ public class StudentConfig {
     @Bean
     public ProducerFactory<String, Long> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
