5b70e543137082c5a9629eac0817f5f6d0c0e795
guymn <117151853+guymn@users.noreply.github.com>
2023-09-16 02:49:17 +0700

    Update CategoryController.java
    delete imp

--- backend/mong-course/src/main/java/spring/edu/mongcourse/controller/CategoryController.java
+++ backend/mong-course/src/main/java/spring/edu/mongcourse/controller/CategoryController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,10 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import spring.edu.mongcourse.ServerMapper;
 import spring.edu.mongcourse.model.Category;
 import spring.edu.mongcourse.model.CategoryDTO;
-import spring.edu.mongcourse.repository.CategoryRepository;
 import spring.edu.mongcourse.service.CategotyControllerService;
 
 @RestController
