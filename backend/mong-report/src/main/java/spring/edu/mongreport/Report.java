9d819109096a8ce143cf802a761391b8c4346bf8
Mizpuno <nititorn.certihunt@gmail.com>
2023-09-15 13:18:53 +0700

    updated Report/ relation on 'student' foreign_key

--- backend/mong-report/src/main/java/spring/edu/mongreport/Report.java
+++ backend/mong-report/src/main/java/spring/edu/mongreport/Report.java
@@ -1,12 +1,9 @@
 package spring.edu.mongreport;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 
 @Entity
--- backend/mong-report/src/main/java/spring/edu/mongreport/ReportDTO.java
+++ backend/mong-report/src/main/java/spring/edu/mongreport/ReportDTO.java
@@ -2,18 +2,31 @@ package spring.edu.mongreport;
 
 public class ReportDTO {
     private Long id;
+    private Long studentId;
     private String description;
 
     public Long getId() {
         return id;
     }
+    
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+    
 }
