0e0d1379701e14e4badb1d748dcdbc26032eb2db
guymn <117151853+guymn@users.noreply.github.com>
2023-09-16 15:04:14 +0700

    add sendMessage

--- backend/mong-course/src/main/java/spring/edu/mongcourse/kafka/KafkaService.java
+++ backend/mong-course/src/main/java/spring/edu/mongcourse/kafka/KafkaService.java
@@ -8,10 +8,15 @@ import org.springframework.stereotype.Service;
 public class KafkaService {
     private final KafkaTemplate<String, Long> kafkaTemplate;
 
+    private final String TOPIC_LESSON = "lessson";
+
     @Autowired
     public KafkaService(KafkaTemplate<String, Long> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
+    public void sendMessage(Long id){
+        kafkaTemplate.send(TOPIC_LESSON, id);
+    }
     
 }
