4a15802aefaa0b8fc1832b8b72c248e7b2156ee8
guymn <117151853+guymn@users.noreply.github.com>
2023-09-16 14:59:59 +0700

    add Autowired in kafkaservice

--- backend/mong-course/src/main/java/spring/edu/mongcourse/kafka/KafkaService.java
+++ backend/mong-course/src/main/java/spring/edu/mongcourse/kafka/KafkaService.java
@@ -1,8 +1,17 @@
 package spring.edu.mongcourse.kafka;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaService {
+    private final KafkaTemplate<String, Long> kafkaTemplate;
+
+    @Autowired
+    public KafkaService(KafkaTemplate<String, Long> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     
 }
--- backend/mong-lesson/src/main/java/spring/edu/monglesson/kafka/KafkaService.java
+++ backend/mong-lesson/src/main/java/spring/edu/monglesson/kafka/KafkaService.java
@@ -1,8 +1,15 @@
 package spring.edu.monglesson.kafka;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaService {
-    
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 }
