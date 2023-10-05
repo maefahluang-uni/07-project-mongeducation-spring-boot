81bc036bff88763a2cbbe588729645c59b78a0dd
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-05 12:01:18 +0700

    update kafka/ config & service to recieved id

--- backend/mong-report/src/main/java/spring/edu/mongreport/kafka/ReportConfig.java
+++ backend/mong-report/src/main/java/spring/edu/mongreport/kafka/ReportConfig.java
@@ -1,6 +1,7 @@
 package spring.edu.mongreport.kafka;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -11,28 +12,28 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @EnableKafka
 @Configuration
 public class ReportConfig {
     @Bean
-	public ConsumerFactory<String, String> consumerFactory() {
-	    Map<String, Object> config = new HashMap<>();
+	public ConsumerFactory<String, List<Integer>> consumerFactory() {
+		Map<String, Object> config = new HashMap<>();
 		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-	    config.put(ConsumerConfig.GROUP_ID_CONFIG,  "student.regist");
-		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		config.put(ConsumerConfig.GROUP_ID_CONFIG,  "student.regist");
+		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
-	    return new DefaultKafkaConsumerFactory<>(config);
+		return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(List.class));
 	}
-    
-    @Bean
-	public ConcurrentKafkaListenerContainerFactory<String, String> 
-	    kafkaListenerContainerFactory() {
-	   
-	    ConcurrentKafkaListenerContainerFactory<String, String> factory =
-	        new ConcurrentKafkaListenerContainerFactory<>();
-	    factory.setConsumerFactory(consumerFactory());
-	    return factory;
-	}    
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, List<Integer>> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, List<Integer>> factory =
+			new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		return factory;
+	}
 }
+
--- backend/mong-report/src/main/java/spring/edu/mongreport/kafka/ReportConsumer.java
+++ backend/mong-report/src/main/java/spring/edu/mongreport/kafka/ReportConsumer.java
@@ -2,6 +2,8 @@ package spring.edu.mongreport.kafka;
 
 import spring.edu.mongreport.ReportController;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -13,8 +15,12 @@ public class ReportConsumer {
     private ReportController reportController;
 
     // recieved data from student entity class.
-    @KafkaListener (topics = "student.regist")
-    void consumeStudentCreateEvent(String studentId) {
-        reportController.createReports(studentId);
+    @KafkaListener(topics = "student.regist")
+    void consumeCreateReportEvent(List<Integer> recievePackaged) {
+        reportController.createReportByEnroll(
+            Integer.toString(recievePackaged.get(0)), 
+            Integer.toString(recievePackaged.get(1))
+        );
     }
+
 }
