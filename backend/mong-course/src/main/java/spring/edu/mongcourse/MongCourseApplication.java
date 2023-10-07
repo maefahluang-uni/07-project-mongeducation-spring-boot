3b59f7bad92e29db1cb463e59d49dd836b0fa293
guymn <117151853+guymn@users.noreply.github.com>
2023-10-08 01:45:42 +0700

    set defult courses

--- backend/mong-course/src/main/java/spring/edu/mongcourse/MongCourseApplication.java
+++ backend/mong-course/src/main/java/spring/edu/mongcourse/MongCourseApplication.java
@@ -1,10 +1,14 @@
 package spring.edu.mongcourse;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import spring.edu.mongcourse.model.Category;
+import spring.edu.mongcourse.repository.CategoryRepository;
 
 @SpringBootApplication
 public class MongCourseApplication {
@@ -20,6 +24,29 @@ public class MongCourseApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**");
 			}
+		};
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(CategoryRepository categoryRepository) {
+		return args -> {
+			Category Mathematics = new Category(null, "Mathematics");
+			categoryRepository.save(Mathematics);
+
+			Category Science = new Category(null, "Science");
+			categoryRepository.save(Science);
+
+			Category History = new Category(null, "History");
+			categoryRepository.save(History);
+
+			Category Literature = new Category(null, "Literature");
+			categoryRepository.save(Literature);
+
+			Category Psychology = new Category(null, "Psychology");
+			categoryRepository.save(Psychology);
+
+			Category Sociology = new Category(null, "Sociology");
+			categoryRepository.save(Sociology);
 		};
 	}
 
